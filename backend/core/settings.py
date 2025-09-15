4933c552244cc72cfcc8fabcf0afde586401c7ae
pranavkp71 <pranavkp170@gmail.com>
2025-09-15 16:29:18 +0530

    Add UserProfile Project and Milestone models

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -21,7 +21,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+4tal03d*)w@vmdix2dn_yvq#*cd)mw3f+)gcn*(t%djt%nhdj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- backend/projects/models.py
+++ backend/projects/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+User = settings.AUTH_USER_MODEL
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    skills = models.TextField(blank=True)
+    github_username = models.CharField(max_length=200, blank=True)
+    availability = models.CharField(max_length=64, blank=True)  # Show if free/busy etc.
+
+class Project(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    owner = models.ForeignKey(User, on_delete=models.CASCADE, related_name="owned_projects")
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_ai_suggested = models.BooleanField(default=False)
+    repo_url = models.URLField(blank=True, null=True)
+
+class Milestone(models.Model):
+    project = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='milestone')
+    title = models.CharField(max_length=200)
+    due_date = models.DateField(null=True, blank=True)
+    order = models.PositiveIntegerField(default=0)
+
+
+
