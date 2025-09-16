fcdb2d3c363d147aceb0391d4477f8ef1d78c6ef
pranavkp71 <pranavkp170@gmail.com>
2025-09-16 21:45:59 +0530

    Add UserProfileSerializer and MilestonSerializer

--- backend/projects/serializers.py
+++ backend/projects/serializers.py
@@ -2,6 +2,17 @@ from rest_framework import serializers
 from django.contrib.auth import get_user_model
 from .models import User, UserProfile, Project, Milestone, Task, ProgressReport
 
-class UserSerializers(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     model = User
     fileds = ['id', 'username', 'email']
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+    class Meta:
+        model = UserProfile
+        fileds = ['user', 'skills', 'github_username', 'availability']
+
+class MilestoneSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Milestone
+        fields = ['id', 'title', 'due_date', 'order']
